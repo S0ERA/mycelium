534db92fbe35f03a790d6f84279a7c67659f6781
SOERA <kereluy@gmail.com>
2025-02-22 01:39:59 +0500

    update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,8 +2,60 @@ import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 
 function Register() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (localStorage.getItem("user")) {
+            navigate('/home');
+        }
+    }, [navigate])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!name || !email || !password) {
+            alert("Все поля обязательны!");
+            return;
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            alert("Некорректный email!");
+            return;
+        }
+        localStorage.setItem("user", JSON.stringify({name, email, password}));
+        navigate("/home");
+    }
+
     return (
-        <h1>Регистрация</h1>
+        <>
+            <h2>Регистрация</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="name"
+                    value={name}
+                    onChange={(e) =>
+                        setName(e.target.value)
+                    } required/>
+                <input
+                    type="email"
+                    placeholder="email"
+                    value={email}
+                    onChange={(e) =>
+                        setEmail(e.target.value)
+                    } required/>
+                <input
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={(e) =>
+                        setPassword(e.target.value)
+                    } required/>
+                <button type="submit">Зарегистрироваться</button>
+            </form>
+        </>
     )
 }
 
